8be7db5ecc5c0e3f30bfba4b1f82278d33bf86ea
ddang770 <nguyendacdang138@gmail.com>
2026-09-09 09:43:05 -0700

    add exercise 2.10, 2.11

---
+++ BaiTapChuong2/Bai2_10/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Text.RegularExpressions;
+
+string text = "banana";
+string substring = "ana";
+Console.WriteLine($"'{substring}' xuat hien {countSubstringOverlapping(text, substring)} lan");
+
+static int countSubstringOverlapping(string text, string substring)
+{
+    if(string.IsNullOrEmpty(text)|| string.IsNullOrEmpty(substring))
+        return 0;
+    return Regex.Matches(text, $"(?={Regex.Escape(substring)})").Count();
+}
---
+++ BaiTapChuong2/Bai2_11/Program.cs
@@ -0,0 +1,36 @@
+﻿
+string correctUsername = "admin";
+string correctPassword = "123";
+int attempts = 0;
+int maxAttempts = 3;
+bool isAuthenticated = false;
+while (attempts<maxAttempts)
+{
+    Console.WriteLine($"Lan thu {attempts+1}/{maxAttempts}");
+    Console.Write("Nhap username: ");
+    string username = Console.ReadLine();
+    Console.Write("Nhap password: ");
+    string password = Console.ReadLine();
+    if (username == correctUsername && password == correctPassword)
+    {
+        isAuthenticated = true;
+        break;
+    }
+    attempts++;
+    int remaining = maxAttempts - attempts;
+
+    if (remaining > 0)
+    {
+        Console.WriteLine($"Thong tin ko chinh xac, ban con {remaining} lan thu");
+    }
+}
+
+// Kết quả sau khi rời vòng lặp
+if (isAuthenticated)
+{
+    Console.WriteLine("Dang nhap thanh cong!");
+}
+else
+{
+    Console.WriteLine("Ban da nhap sai qua 3 lan. Dong chuong trinh!");
+}
